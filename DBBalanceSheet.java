1cd8e85fb8d8d9221a0157c30419f03054dabc9c
jlittman1995 <jlittman@iastate.edu>
2016-10-31 20:26:49 -0500

    Add files via upload

---
+++ DBBalanceSheet.java
@@ -0,0 +1,15 @@
+
+public class DBBalanceSheet {
+
+	public static void main(String[] args) 
+	{
+		int totalAssets;
+		int totalLiabilities;
+		int totalSE;
+		if (totalAssets != totalLiabilities + totalSE)
+		{
+			System.err.print("Balance Sheet does not balane");
+		}
+	}
+	public static 
+}
